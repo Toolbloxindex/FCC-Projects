3d6f8ad585fbfbe42f3aa0ddec83696782a7cb53
Toolbloxindex <jfjsc.taba@gmail.com>
2025-07-07 21:29:20 +0100

    optimized version for efficiency and more accuracy using regex

--- FCC  Legacy JavaScript Algorithms and Data Structures/4- Telephone Number Validator/code.js
+++ FCC  Legacy JavaScript Algorithms and Data Structures/4- Telephone Number Validator/code.js
@@ -1,18 +1,6 @@
 function telephoneCheck(str) {
-    let removeRegx = /[^0-9]/g
-    let cleanedStr = str.replace(removeRegx, "")
-    if (cleanedStr.length == 10 || cleanedStr.length == 11) {
-        if (cleanedStr.length == 11 && cleanedStr[0] != "1") {
-            return false
-        }
-        if (cleanedStr.length == 10 && cleanedStr[0] == "1") {
-            return false
-        }
-    }
-    else {
-        return false
-    }
-    return true
+    let regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-])?\d{3}([\s\-])?\d{4}$/;
+    return regex.test(str);
 }
 
 console.log(telephoneCheck("1 555)555-5555"));
