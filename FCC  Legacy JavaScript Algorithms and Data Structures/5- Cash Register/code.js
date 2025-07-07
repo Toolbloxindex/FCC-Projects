7b9535cfe3feacfca67cfb27e4703bb1641173b3
Toolbloxindex <jfjsc.taba@gmail.com>
2025-07-07 23:34:41 +0100

    re-did, optimized and fixed code

--- FCC  Legacy JavaScript Algorithms and Data Structures/5- Cash Register/code.js
+++ FCC  Legacy JavaScript Algorithms and Data Structures/5- Cash Register/code.js
@@ -1,20 +1,17 @@
 function checkCashRegister(price, cash, cid) {
-    let registerCash = {
-        "PENNY": cid[0][1],
-        "NICKEL": cid[1][1],
-        "DIME": cid[2][1],
-        "QUARTER": cid[3][1],
-        "ONE": cid[4][1],
-        "FIVE": cid[5][1],
-        "TEN": cid[6][1],
-        "TWENTY": cid[7][1],
-        "ONE HUNDRED": cid[8][1]
-    }
-    let cashSum = 0;
-    for (let key in registerCash) {
-        cashSum += registerCash[key];
-    }
-    console.log(cashSum)
+    const currencyUnit = {
+    "PENNY": 0.01,
+    "NICKEL": 0.05,
+    "DIME": 0.10,
+    "QUARTER": 0.25,
+    "ONE": 1.00,
+    "FIVE": 5.00,
+    "TEN": 10.00,
+    "TWENTY": 20.00,
+    "ONE HUNDRED": 100.00
+    };
+    let cashSum = parseFloat(cid.reduce((sum, curr) => sum + curr[1], 0).toFixed(2));
+
     let change = cash - price;
     if (change > cashSum) {
         return {status: "INSUFFICIENT_FUNDS", change: []}
@@ -23,19 +20,25 @@ function checkCashRegister(price, cash, cid) {
         return {status: "CLOSED", change: [...cid]}
     }
     
-    for (let key in registerCash) {
-      while (change >= 0) {
-        change -= registerCash[key];
-      }
+    let reversedCid = cid.reverse();
+    let changeArr = [];
+    for (let [unit, amount] of reversedCid) {
+        let unitValue = currencyUnit[unit]
+        let used = 0 
+        while (change >= unitValue && amount > 0) {
+            change  -= unitValue
+            change = parseFloat(change.toFixed(2))
+            amount -= unitValue
+            used += unitValue
+        }
+        if (used > 0) {
+            changeArr.push([unit, used])
+        }
     }
-    //CHECK IF THERE IS EXACT CHANGE
-    if (change < 0) {
-        return {status: "INSUFFICIENT_FUNDS", change: []}
+    if (change > 0) {
+            return { status: "INSUFFICIENT_FUNDS", change: [] };
     }
-    else {
-        return 
-    }
-    
+    return {status: "OPEN", change: [...changeArr]}
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
